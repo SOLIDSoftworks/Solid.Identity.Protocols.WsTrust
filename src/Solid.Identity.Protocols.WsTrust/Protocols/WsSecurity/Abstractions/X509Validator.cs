b14c9fd61e63e57d5f90d889a14a018854fd5152
Gísli Konráð Björnsson <gisli.konrad@gmail.com>
2020-08-21 12:53:09 +0000

    changes claims added from X509Validation

--- src/Solid.Identity.Protocols.WsTrust/Protocols/WsSecurity/Abstractions/X509Validator.cs
+++ src/Solid.Identity.Protocols.WsTrust/Protocols/WsSecurity/Abstractions/X509Validator.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Security.Claims;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Solid.Identity.Protocols.WsSecurity.Abstractions
@@ -12,13 +13,14 @@ namespace Solid.Identity.Protocols.WsSecurity.Abstractions
     {
         public static readonly string AuthenticationType = "X509";
         public static readonly string AuthenticationMethod = "http://schemas.microsoft.com/ws/2008/06/identity/authenticationmethod/x509";
+        public static readonly Regex CommonNameRegex = new Regex("CN=([^ ])*?");
 
         public async ValueTask<ClaimsPrincipal> ValidateCertificateAsync(X509Certificate2 certificate)
         {
             if (!await IsValidAsync(certificate)) return null;
 
-            var subject = await GetSubjectAsync(certificate);
-            var claims = await GenerateClaimsAsync(subject, certificate);
+            var name = await GetCommonNameAsync(certificate);
+            var claims = await GenerateClaimsAsync(name, certificate);
             var identity = await CreateIdentityAsync(claims);
             return new ClaimsPrincipal(identity);
         }
@@ -28,16 +30,20 @@ namespace Solid.Identity.Protocols.WsSecurity.Abstractions
             var identity = new ClaimsIdentity(claims, AuthenticationType);
             return new ValueTask<ClaimsIdentity>(identity);
         }
-        
-        protected virtual ValueTask<string> GetSubjectAsync(X509Certificate2 certificate)
-            => new ValueTask<string>(certificate.Subject);
 
-        protected virtual ValueTask<IEnumerable<Claim>> GenerateClaimsAsync(string subject, X509Certificate2 certificate)
+        protected virtual ValueTask<string> GetCommonNameAsync(X509Certificate2 certificate)
+        {
+            var match = CommonNameRegex.Match(certificate.Subject);
+            if (!match.Success) return new ValueTask<string>(certificate.Subject);
+            return new ValueTask<string>(match.Groups[1].Value);
+        }
+
+        protected virtual ValueTask<IEnumerable<Claim>> GenerateClaimsAsync(string name, X509Certificate2 certificate)
         {
             var claims = new[]
             {
-                new Claim(ClaimTypes.NameIdentifier, subject),
-                new Claim(ClaimTypes.Name, certificate.Subject),
+                new Claim(ClaimTypes.NameIdentifier, certificate.Thumbprint),
+                new Claim(ClaimTypes.Name, name),
                 new Claim(ClaimTypes.AuthenticationMethod, AuthenticationMethod),
                 new Claim(ClaimTypes.X500DistinguishedName, certificate.Subject),
                 AuthenticationInstantClaim.Now
