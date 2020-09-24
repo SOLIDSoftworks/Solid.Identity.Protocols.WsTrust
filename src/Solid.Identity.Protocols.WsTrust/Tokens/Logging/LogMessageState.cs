5feedd9fcd546f14ab5f6daa79b1e2da35cd7ee8
Gísli Konráð Björnsson <gisli.konrad@gmail.com>
2020-09-24 15:29:37 +0000

    Update LogMessageState.cs

--- src/Solid.Identity.Protocols.WsTrust/Tokens/Logging/LogMessageState.cs
+++ src/Solid.Identity.Protocols.WsTrust/Tokens/Logging/LogMessageState.cs
@@ -5,8 +5,15 @@ using System.Text.Json;
 
 namespace Solid.Identity.Tokens.Logging
 {
-    internal class LogMessageState
+    /// <summary>
+    /// A base class used for log state
+    /// </summary>
+    public abstract class LogMessageState
     {
+        /// <summary>
+        /// JSON serializes the class with indents
+        /// </summary>
+        /// <returns>An indented JSON representation of the current class.</returns>
         public override string ToString() => JsonSerializer.Serialize(this, GetType(), new JsonSerializerOptions { WriteIndented = true });
     }
 }
