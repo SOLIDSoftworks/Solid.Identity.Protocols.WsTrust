3567387f8536dc75c6cd75ba2126d4b22b4e4d5e
Gísli Konráð Björnsson <gisli.konrad@gmail.com>
2020-10-13 17:26:57 +0000

    Update SecurityTokenHandlerWrapper.cs

--- src/Solid.Identity.Protocols.WsTrust/Tokens/SecurityTokenHandlerWrapper.cs
+++ src/Solid.Identity.Protocols.WsTrust/Tokens/SecurityTokenHandlerWrapper.cs
@@ -11,57 +11,67 @@ namespace Solid.Identity.Tokens
 {
     internal class SecurityTokenHandlerWrapper : AsyncSecurityTokenHandler
     {
-        private SecurityTokenHandler _inner;
+        public SecurityTokenHandler Inner { get; }
 
-        public SecurityTokenHandlerWrapper(SecurityTokenHandler inner) => _inner = inner;
+        public SecurityTokenHandlerWrapper(SecurityTokenHandler inner) => Inner = inner;
 
-        public override Type TokenType => _inner.TokenType;
+        public override Type TokenType => Inner.TokenType;
 
-        public override int MaximumTokenSizeInBytes { get => _inner.MaximumTokenSizeInBytes; set => _inner.MaximumTokenSizeInBytes = value; }
+        public override int MaximumTokenSizeInBytes { get => Inner.MaximumTokenSizeInBytes; set => Inner.MaximumTokenSizeInBytes = value; }
 
-        public override bool CanValidateToken => _inner.CanValidateToken;
+        public override bool CanValidateToken => Inner.CanValidateToken;
 
-        public override bool CanWriteToken => _inner.CanWriteToken;
+        public override bool CanWriteToken => Inner.CanWriteToken;
 
-        public override bool CanReadToken(XmlReader reader) => _inner.CanReadToken(reader);
+        public override bool CanReadToken(XmlReader reader) => Inner.CanReadToken(reader);
 
-        public override bool CanReadToken(string tokenString) => _inner.CanReadToken(tokenString);
+        public override bool CanReadToken(string tokenString) => Inner.CanReadToken(tokenString);
         
         public override bool CanWriteSecurityToken(SecurityToken securityToken)
         {
-            if (_inner.CanWriteSecurityToken(securityToken)) return true;
+            if (Inner.CanWriteSecurityToken(securityToken)) return true;
             return base.CanWriteSecurityToken(securityToken);
         }
 
-        public override SecurityKeyIdentifierClause CreateSecurityTokenReference(SecurityToken token, bool attached) => _inner.CreateSecurityTokenReference(token, attached);
+        public override SecurityKeyIdentifierClause CreateSecurityTokenReference(SecurityToken token, bool attached)
+        {
+            try
+            {
+                return Inner.CreateSecurityTokenReference(token, attached);
+            }
+            catch
+            {
+                return null;
+            }
+        }
 
-        public override SecurityToken CreateToken(SecurityTokenDescriptor tokenDescriptor) => _inner.CreateToken(tokenDescriptor);
+        public override SecurityToken CreateToken(SecurityTokenDescriptor tokenDescriptor) => Inner.CreateToken(tokenDescriptor);
 
         public override bool Equals(object obj)
         {
             if (obj is SecurityTokenHandlerWrapper wrapper)
-                return _inner.Equals(wrapper._inner);
-            return _inner.Equals(obj);
+                return Inner.Equals(wrapper.Inner);
+            return Inner.Equals(obj);
         }
 
-        public override int GetHashCode() => _inner.GetHashCode();
+        public override int GetHashCode() => Inner.GetHashCode();
 
-        public override SecurityToken ReadToken(XmlReader reader, TokenValidationParameters validationParameters) => _inner.ReadToken(reader, validationParameters);
+        public override SecurityToken ReadToken(XmlReader reader, TokenValidationParameters validationParameters) => Inner.ReadToken(reader, validationParameters);
 
-        public override SecurityToken ReadToken(string tokenString) => _inner.ReadToken(tokenString);
+        public override SecurityToken ReadToken(string tokenString) => Inner.ReadToken(tokenString);
 
-        public override SecurityToken ReadToken(XmlReader reader) => _inner.ReadToken(reader);
+        public override SecurityToken ReadToken(XmlReader reader) => Inner.ReadToken(reader);
 
-        public override string ToString() => _inner.ToString();
+        public override string ToString() => Inner.ToString();
 
-        public override bool TryWriteSourceData(XmlWriter writer, SecurityToken securityToken) => _inner.TryWriteSourceData(writer, securityToken);
+        public override bool TryWriteSourceData(XmlWriter writer, SecurityToken securityToken) => Inner.TryWriteSourceData(writer, securityToken);
 
-        public override ClaimsPrincipal ValidateToken(string securityToken, TokenValidationParameters validationParameters, out SecurityToken validatedToken) => _inner.ValidateToken(securityToken, validationParameters, out validatedToken);
+        public override ClaimsPrincipal ValidateToken(string securityToken, TokenValidationParameters validationParameters, out SecurityToken validatedToken) => Inner.ValidateToken(securityToken, validationParameters, out validatedToken);
 
-        public override ClaimsPrincipal ValidateToken(XmlReader reader, TokenValidationParameters validationParameters, out SecurityToken validatedToken) => _inner.ValidateToken(reader, validationParameters, out validatedToken);
+        public override ClaimsPrincipal ValidateToken(XmlReader reader, TokenValidationParameters validationParameters, out SecurityToken validatedToken) => Inner.ValidateToken(reader, validationParameters, out validatedToken);
 
-        public override void WriteToken(XmlWriter writer, SecurityToken token) => _inner.WriteToken(writer, token);
+        public override void WriteToken(XmlWriter writer, SecurityToken token) => Inner.WriteToken(writer, token);
 
-        public override string WriteToken(SecurityToken token) => _inner.WriteToken(token);
-    }    
+        public override string WriteToken(SecurityToken token) => Inner.WriteToken(token);
+    }
 }
